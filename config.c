90eb0bcfe7971964b518584468bdb2a41bae504e
not-kaz <kazislav@protonmail.com>
2023-04-15 17:06:36 +0200

    Stamped out conversion warnings w/ explicit casts.

--- config.c
+++ config.c
@@ -59,16 +59,15 @@ static struct config_entry *find_entry(struct config_entry *entry, char *name)
 void config_entry_register(struct config_entry *entry, char *name, int value)
 {
 	struct config_entry *found;
-	int val;
+	int restored;
 
 	if ((found = find_entry(entry, name))) {
 		LOG_INFO("Config entry '%s' already registered.", found->name);
 		return;
 	}
-	val = (intptr_t)(hash_map_at(registry, name));
-	value = val ? val : value;
+	restored = (int)(intptr_t)(hash_map_at(registry, name));
 	entry->name = name;
-	entry->value = value;
+	entry->value = restored ? restored : value;
 	entry->next = entries;
 	entries = entry;
 }
@@ -92,13 +91,13 @@ void config_load(void)
 	fseek(fp, 0, SEEK_END);
 	len = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
-	str = malloc(len + 1);
+	str = malloc((size_t)(len + 1));
 	if (!str) {
 		LOG_WARN("Failed at reading config file.");
 		fclose(fp);
 		return;
 	}
-	fread(str, len, 1, fp);
+	fread(str, (size_t)(len), 1, fp);
 	str[len] = '\0';
 	parse_config_text(str);
 	free(str);
