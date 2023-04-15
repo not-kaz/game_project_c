b2ad8101985e6a1b15d1d894197ed10519b8ee25
not-kaz <kazislav@protonmail.com>
2023-04-15 16:36:06 +0200

    Moved from fixed array to string hash map.

--- config.c
+++ config.c
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include "common.h"
+#include "hash_map.h"
 #include "log.h"
 
 #include <stdio.h>
@@ -9,18 +10,12 @@
 
 #define MAX_SAVED_ENTRIES 256
 
-/* TODO: Move to dynamic array or hash table. */
-static struct {
-	char *name;
-	char *value;
-} saved_entries[MAX_SAVED_ENTRIES];
-
-static struct config_entry *config_entries;
-static const char *config_filename = "config.cfg";
+static struct hash_map *registry;
+static struct config_entry *entries;
+static const char *filename = "config.cfg";
 
 static void parse_config_line(char *ln)
 {
-	/* TODO: Find a way to avoid memory allocation w/ str_duplicate() */
 	char *name, *val;
 
 	name = str_split(&ln, "=");
@@ -31,13 +26,8 @@ static void parse_config_line(char *ln)
 	if (!val) {
 		return;
 	}
-	for (int i = 0; i < MAX_SAVED_ENTRIES; i++) {
-		if (!saved_entries[i].name || !saved_entries[i].value) {
-			saved_entries[i].name = str_duplicate(name);
-			saved_entries[i].value = str_duplicate(val);
-			break;
-		}
-	}
+	/* TODO: Move from atoi() to strtol(). Safer. */
+	hash_map_insert(registry, name, (void *)(intptr_t)(atoi(val)));
 }
 
 static void parse_config_text(char *str)
@@ -60,7 +50,7 @@ static struct config_entry *find_entry(struct config_entry *entry, char *name)
 {
 	struct config_entry *curr;
 
-	for (curr = config_entries; curr; curr = curr->next) {
+	for (curr = entries; curr; curr = curr->next) {
 		if ((!strcmp(curr->name, name)) || (curr == entry)) {
 			return curr;
 		}
@@ -71,36 +61,19 @@ static struct config_entry *find_entry(struct config_entry *entry, char *name)
 void config_entry_register(struct config_entry *entry, char *name, int value)
 {
 	struct config_entry *found;
+	int val;
 
 	if ((found = find_entry(entry, name))) {
 		LOG_INFO("Config entry '%s' already registered.", found->name);
 		return;
 	}
-	for (int i = 0; i < MAX_SAVED_ENTRIES; i++) {
-		/* OPTIMIZE: String comparing is not very performant. */
-		if (!saved_entries[i].name && !saved_entries[i].value) {
-			break;
-		}
-		if (!strcmp(name, saved_entries[i].name)) {
-			int val;
-			char *end;
-
-			/* OPTIMIZE: If values are the same, don't reassign. */
-			val = strtol(saved_entries[i].value, &end, 10);
-			if (*end != '\0') {
-				LOG_WARN("Input from config file was invalid.");
-				LOG_INFO("%d", val);
-				break;
-			}
-			value = val;
-			LOG_INFO("Loaded new value from file, %s=%d", name,
-				value);
-		}
+	if ((val = (intptr_t)(hash_map_at(registry, name)))) {
+		value = val;
 	}
 	entry->name = name;
 	entry->value = value;
-	entry->next = config_entries;
-	config_entries = entry;
+	entry->next = entries;
+	entries = entry;
 }
 
 void config_load(void)
@@ -109,7 +82,12 @@ void config_load(void)
 	long len;
 	char *str;
 
-	fp = fopen(config_filename, "r");
+	registry = hash_map_create();
+	if (!registry) {
+		LOG_ERROR("Failed to create config registry.");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(filename, "r");
 	if (!fp) {
 		LOG_WARN("Failed to load config. Falling back to defaults.");
 		return;
@@ -135,30 +113,21 @@ void config_save(void)
 	FILE *fp;
 	struct config_entry *curr;
 
-	fp = fopen(config_filename, "w");
+	fp = fopen(filename, "w");
 	if (!fp) {
 		LOG_WARN("Failed at writing config file to disk.");
 		return;
 	}
-	for (curr = config_entries; curr; curr = curr->next) {
+	for (curr = entries; curr; curr = curr->next) {
 		/* TODO: Find out if overwriting everything is faster than
 		 * parsing through and finding what to overwrite. */
 		fprintf(fp, "%s=%d\n", curr->name, curr->value);
 	}
-	LOG_INFO("Saved current settings to config file '%s'", config_filename);
+	LOG_INFO("Saved current settings to config file '%s'", filename);
 	fclose(fp);
 }
 
 void config_free(void)
 {
-	for (int i = 0; i < MAX_SAVED_ENTRIES; i++) {
-		/* IMPROVE: Reset all pointers to NULL to prevent UB if somebody
-		 * for some reason accesses data after free(). */
-		if (!saved_entries[i].name || !saved_entries[i].value) {
-			break;
-		}
-		free(saved_entries[i].name);
-		free(saved_entries[i].value);
-		saved_entries[i].name = NULL;
-	}
+	hash_map_destroy(registry);
 }
